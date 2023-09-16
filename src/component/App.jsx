6aebd93650c727026d8f209b17483bac4eedc475
Tristan Tourbier <tristan.tourbier@outlook.fr>
2023-09-16 19:45:51 +0200

    fond d'écran ajouté

---
+++ src/component/App.jsx
@@ -0,0 +1,22 @@
+import '../css/App.css';
+
+var body = document.body;
+
+  body.style.backgroundImage = 'url("Wallpaper.webp"';
+  body.style.backgroundSize = 'cover';
+  body.style.backgroundRepeat = 'no-repeat';
+  body.style.backgroundAttachment = 'fixed';
+  body.style.backgroundAttachment = 'center top';
+  body.style.height = '100vh';
+  body.style.margin = '0';
+  //body.style.minWidth = '100px';
+
+function App() {
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default App;
